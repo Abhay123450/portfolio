ca1ffc16d62f75fb9d030fae2430efb4ee46bc0b
Abhay123450 <95590546+Abhay123450@users.noreply.github.com>
2024-09-26 11:36:20 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <div className="bg-slate-800 text-red-400">Hello</div>
+        </>
+    );
+}
+
+export default App;
